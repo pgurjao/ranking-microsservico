698a1ff2dc5f1be50db125c8788d9cf50597fe23
Paulo José Gurjão <paulogurjao.gft@sulamerica.com.br>
2022-01-24 16:13:46 -0300

    Commit inicial 1

---
+++ src/main/java/br/edu/infnet/rankingmicrosservico/RankingMicrosservicoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.rankingmicrosservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankingMicrosservicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankingMicrosservicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/rankingmicrosservico/RankingMicrosservicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.rankingmicrosservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankingMicrosservicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
