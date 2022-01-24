41bfc79ebb655ce415cb7a51c17f2e22eef31657
Paulo José Gurjão <paulogurjao.gft@sulamerica.com.br>
2022-01-24 17:50:34 -0300

    Deletando data.sql para poder subir a aplicação

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
--- Descrição da tabela TURNO: id, atacante, batalha_id, dano , heroi, monstro, usuario
--- INSERT INTO `turno`(id, atacante, batalha_id, dano, heroi, monstro, usuario) VALUES (1,0,323111,3,'Guerreiro','Morto Vivo','Zombie Slayer');
--- INSERT INTO `turno`(id, atacante, batalha_id, dano, heroi, monstro, usuario) VALUES (2,1,323111,2,'Guerreiro','Morto Vivo','Zombie Slayer');
--- INSERT INTO `turno`(id, atacante, batalha_id, dano, heroi, monstro, usuario) VALUES (3,0,323111,5,'Guerreiro','Morto Vivo','Zombie Slayer');
--- INSERT INTO `turno`(id, atacante, batalha_id, dano, heroi, monstro, usuario) VALUES (4,0,654321,4,'Bárbaro','Orc','Orc Killer');
--- INSERT INTO `turno`(id, atacante, batalha_id, dano, heroi, monstro, usuario) VALUES (5,0,555777,7,'Paladino','Kobold','Bruninha Destruidora');
--- INSERT INTO `turno`(id, atacante, batalha_id, dano, heroi, monstro, usuario) VALUES (6,1,654321,8,'Bárbaro','Orc','Orc Killer');
